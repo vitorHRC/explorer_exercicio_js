63d59190c683ab4702d4e6e3e27e767d3ec981b7
Vitor Henrique <108952408+vitorHRC@users.noreply.github.com>
2024-01-28 22:31:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+// 1)
+alert('Hello World!')
+
+// 2)
+const numberOne = 10
+const numberTwo = 2
+
+const sum = numberOne + numberTwo
+
+alert(sum)
+
+//3)
+
+const isNumber = 2
+
+if (typeof isNumber == 'number') {
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+
+//4)
+const isString = 'hello'
+
+if (typeof isString == 'string') {
+  alert('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
+// 5)
+
+const isBoolean = true
+
+if (typeof isBoolean == 'boolean') {
+  alert('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
+
+// 6)
+
+const sub = numberOne - numberTwo
+
+alert(sub)
+
+// 7)
+
+const mult = numberOne * numberTwo
+
+alert(mult)
+
+// 8)
+
+const div = numberOne / numberTwo
+
+alert(div)
+
+// 9)
+
+const isEvenNumber = 4
+
+if (isEvenNumber % 2 == 0) {
+  alert('É um número par')
+} else {
+  alert('Não é um número par')
+}
+
+// 10)
+
+const isOddNumber = 5
+
+if (isOddNumber % 2 != 0) {
+  alert('É um número ímpar')
+} else {
+  alert('Não é um número ímpar')
+}
